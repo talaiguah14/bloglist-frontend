daaa7c631a29ee024b708a8e7552ce9ac7cf4817
CristianTalaiguaNRA <cristian.talaigua@numrot.com>
2024-01-31 16:55:56 -0500

    Feat: Blog List Frontend Startup, Step 1

---
+++ src/components/Login.jsx
@@ -0,0 +1,26 @@
+import Input from "./atoms/input/Input"
+
+const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <Input/>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
